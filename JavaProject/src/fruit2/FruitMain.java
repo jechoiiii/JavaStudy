026be2ee11364b68a500b9ebbfce2b9c0004673e
jechoiiii <71616776+jechoiiii@users.noreply.github.com>
2020-10-07 18:17:25 +0900

    Update JavaProject

---
+++ JavaProject/src/fruit2/FruitMain.java
@@ -0,0 +1,24 @@
+package fruit2;
+
+public class FruitMain {
+
+	public static void main(String[] args) {
+	
+	/*	FruitSeller seller1 = new FruitSeller();		// 인스턴스화
+		seller1.initMembers(0, 30, 1500);
+		
+		FruitBuyer seller2 = new FruitBuyer();
+		seller2.initMembers(0,20,1000);
+		
+		buyer.buyApple(seller1, 4500);
+		buyer.buyApple(seller2, 3000);
+		
+		System.out.println("과일 판매자1의 현재 상황");
+		seller1.showSaleResult();
+		
+		System.out.println();
+	*/	
+
+	}
+
+}
