94a1e3aa05d82391eb5396b2b24a037cbbdad2ca
jechoiiii <jechoiiii@gmail.com>
2021-01-14 11:24:39 +0900

    2021.01.14 Spring 오픈프로젝트 - 회원 리스트

---
+++ spring_project/Spring-OpenProject/src/main/java/com/aia/op/member/domain/MemberListView.java
@@ -0,0 +1,64 @@
+package com.aia.op.member.domain;
+
+import java.util.List;
+
+public class MemberListView {
+
+	private int pagenumber; 	// 현재 페이지 번호 
+	private int totalMemberCount; // 전체 게시물의 개수 
+	private int cntPerPage; 	// 한 페이지에 노출할 게시물의 개수 
+	private List<Member> memberList; // 페이지에 노출할 회원의 정보를 담는 리스트 
+	private int startRow;
+	private int endRow;
+	int totalPageCount;			// 전체 페이지의 개수 
+	
+	public MemberListView(int pagenumber, int totalMemberCount, int cntPerPage, List<Member> memberList, int startRow,
+			int endRow) {
+		this.pagenumber = pagenumber;
+		this.totalMemberCount = totalMemberCount;
+		this.cntPerPage = cntPerPage;
+		this.memberList = memberList;
+		this.startRow = startRow;
+		this.endRow = endRow;
+		calTotalPageCount();
+	}
+
+	private void calTotalPageCount() {
+		
+		totalPageCount = totalMemberCount/cntPerPage;
+		if(totalMemberCount%cntPerPage >0) {
+			totalPageCount++;
+		}
+	}
+
+	public int getPagenumber() {
+		return pagenumber;
+	}
+	public int getTotalMemberCount() {
+		return totalMemberCount;
+	}
+	public int getCntPerPage() {
+		return cntPerPage;
+	}
+	public List<Member> getMemberList() {
+		return memberList;
+	}
+	public int getStartRow() {
+		return startRow;
+	}
+	public int getEndRow() {
+		return endRow;
+	}
+	public int getTotalPageCount() {
+		return totalPageCount;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberListView [pagenumber=" + pagenumber + ", totalMemberCount=" + totalMemberCount + ", cntPerPage="
+				+ cntPerPage + ", memberList=" + memberList + ", startRow=" + startRow + ", endRow=" + endRow
+				+ ", totalPageCount=" + totalPageCount + "]";
+	}
+	
+	
+}
